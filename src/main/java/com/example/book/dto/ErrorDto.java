3d33ede1fa157c1dc3b129283766c1a1f8d5c7ff
Sayan Ghosh <iamsayan3641@gmail.com>
2026-02-22 00:03:53 +0530

    Global exception handler added

---
+++ src/main/java/com/example/book/dto/ErrorDto.java
@@ -0,0 +1,13 @@
+package com.example.book.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorDto {
+	
+	private String title;
+	private String description;
+
+}
